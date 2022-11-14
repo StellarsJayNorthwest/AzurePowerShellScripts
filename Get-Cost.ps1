cc251792d3eda5f315299f0671e05c2cf7592a70
Jeff Westhead <jeff314159@hotmail.com>
2022-11-14 10:20:25 -0800

    Revise Get-Cost.ps1 to process resource groups

--- Get-Cost.ps1
+++ Get-Cost.ps1
@@ -1,11 +1,13 @@
 [CmdletBinding()]
 Param(
-    [int] $MonthsBackToStart = 6,
-    [int] $MonthsToCollect = 3,
+    [int] $MonthsBackToStart = 12,
+    [int] $MonthsToCollect = 12,
     [string]$OutFile = ".\cost.csv"
 )
 
 Write-Host
+
+$startTime = Get-Date
 
 # Before we start iterating VMs, retrieve the management groups. Create a hash table associating each subscription with
 # the name of a management group. We'll use this hash table to determine which management group each VM belongs to.
@@ -28,55 +30,94 @@ Write-Host
 # Get the list of subscriptions
 $subscriptions = Get-AzSubscription
 
+$today = Get-Date -Format "MM/dd/yyyy"
+
 $csvArray = @()
 
 $monthIndex = 0
 while ($monthIndex -lt $MonthsToCollect) {
 
-    $today = Get-Date -Format "MM/dd/yyyy"
-    $monthStart = (Get-Date $today -Day 1).AddMonths(-1 * $MonthsBackToStart)
-    $monthStart = $monthStart.AddMonths($monthIndex)
+    $monthStart = (Get-Date $today -Day 1).AddMonths(-1 * $MonthsBackToStart + $monthIndex)
     $monthEnd = $monthStart.AddMonths(1).AddSeconds(-1)
 
     foreach ($subscription in $subscriptions) {
         $subscription | Select-AzSubscription | Out-Null
 
+        # Determine the management group name of this subscription.
+        $managementGroupName = "Unknown"
+        if ($managementGroupHashTable.ContainsKey($Subscription.Id)) {
+            $managementGroupName = $managementGroupHashTable[$Subscription.Id]
+        }
+
         Write-Host
-        Write-Host "Gathering cost from $monthStart to $monthEnd for $($subscription.Name)"
+        Write-Host "Gathering cost from $monthStart to $monthEnd for $managementGroupName $($subscription.Name) at $(Get-Date)"
 
         $consumptionDetails = Get-AzConsumptionUsageDetail -StartDate $monthStart -EndDate $monthEnd
 
         # Don't output an entry in the CSV if there were no cost details
         if ($consumptionDetails.Count -ne 0) {
 
-            Write-Host "Retrieved $($consumptionDetails.Count) consumption usage entries for $($subscription.Name)"
+            Write-Host "Retrieved $($consumptionDetails.Count) consumption usage entries for $($subscription.Name) at $(Get-Date)"
 
-            # Determine the management group name of the subscription.
-            $managementGroupName = "Unknown"
-            if ($managementGroupHashTable.ContainsKey($Subscription.Id)) {
-                $managementGroupName = $managementGroupHashTable[$Subscription.Id]
-            }
-
-            $cost = 0
+            # We must now process all of the consumption detail objects. Each detail object contains the name of the
+            # resource group embedded in the very long "InstanceId" string. Create a hash table where the key is the
+            # resource group name and the value is the sum of the costs. 
+            $costHashTable = @{}
             $currency = ""
             foreach ($c in $consumptionDetails) {
-                $cost = $cost + $c.PretaxCost
+
+                # Save the currency string. We assume all details for this subscription use the same currency.
                 $currency = $c.Currency
+
+                if ($c.InstanceId -match "resourceGroups/(.*?)/") {
+
+                    $key = $Matches[1]
+
+                    # For uniformity, convert all resource group names to lower case. This could be commented out.
+                    $key = $key.ToLower()
+
+                    # Write-Host "Cost detail: $key cost $($c.PretaxCost)"
+
+                    # If the key exists, add this cost. Otherwise create a new key with this cost.
+                    if ($costHashTable.ContainsKey($key)) {
+                        $costHashTable[$key] = $costHashTable[$key] + $c.PretaxCost
+                    } else {
+                        $costHashTable[$key] = $c.PretaxCost
+                    }
+
+                } else {
+
+                    Write-Host "Failed to get resource group from InstanceId `'$($c.InstanceId)`'"
+                }
             }
 
-            Write-Host "Pre-tax cost from $monthStart to $monthEnd was $cost in $currency"
+            # Now we have the cost by resource group in $costHashTable. Output one CSV line for each entry $costHashTable.
+            foreach ($resourceGroupName in $costHashTable.Keys) { 
 
-            $row = "" | Select ManagementGroup, SubscriptionName, SubscriptionId, Start, End, Cost, Currency
-            $row.ManagementGroup = $managementGroupName
-            $row.SubscriptionName = $subscription.Name
-            $row.SubscriptionId = $subscription.Id
-            $row.Start = Get-Date $monthStart -Format "MM/dd/yyyy"
-            $row.End = Get-Date $monthEnd -Format "MM/dd/yyyy"
-            $row.Cost = $cost
-            $row.Currency = $currency
-            $csvArray += $row
+                # Determine the location of this resource group by getting it from Az-GetResourceGroup. If the resource
+                # group no longer exists, then use "Unknown" as the location.
+                $location = "Unknown"
+                $rg = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+                if ($rg) {
+                    $location = $rg.Location
+                }
+
+                # Write-Host "Pre-tax cost from $monthStart to $monthEnd for $resourceGroupName in $location was $($costHashTable[$resourceGroupName]) $currency"
+
+                $row = "" | Select ManagementGroup, SubscriptionName, SubscriptionId, ResourceGroup, Location, Start, End, Cost, Currency
+                $row.ManagementGroup = $managementGroupName
+                $row.SubscriptionName = $subscription.Name
+                $row.SubscriptionId = $subscription.Id
+                $row.ResourceGroup = $resourceGroupName
+                $row.Location = $location
+                $row.Start = Get-Date $monthStart -Format "MM/dd/yyyy"
+                $row.End = Get-Date $monthEnd -Format "MM/dd/yyyy"
+                $row.Cost = $costHashTable[$resourceGroupName]
+                $row.Currency = $currency
+                $csvArray += $row
+            }
         } else {
-            Write-Host "Unable to get consumption usage entries for $($subscription.Name)"
+            Write-Host "Unable to get consumption usage entries for $($subscription.Name) at $(Get-Date)"
         }
 
         Write-Host
@@ -88,3 +129,5 @@ while ($monthIndex -lt $MonthsToCollect) {
 
 $csvArray | Export-Csv $OutFile -Force -NoTypeInformation
 Write-Host "Exported $($csvArray.Count) entries to $OutFile"
+Write-Host "Script started at $startTime and finished at $(Get-Date)"
+Write-Host "Script run time: $((Get-Date) - $startTime)"
