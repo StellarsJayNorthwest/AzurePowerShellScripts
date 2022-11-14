b2c144cec9ca0564e34f0637436f54df2f4e4d38
Jeff Westhead <jeff314159@hotmail.com>
2022-11-14 10:19:38 -0800

    Get-MySubscriptions.ps1

---
+++ Get-MySubscriptions.ps1
@@ -0,0 +1,10 @@
+﻿$array = @()
+
+foreach ($subscription in Get-AzSubscription) {
+    $row = "" | Select Name, Id
+    $row.Name = $subscription.Name
+    $row.Id = $subscription.Id
+    $array += $row
+}
+
+$array
